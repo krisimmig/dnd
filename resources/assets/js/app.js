3c915ba3dd0b26b9a6dcc7bf19ba63f7b89bdb99
Kris Immig <kris.immig@gmail.com>
2018-02-11 23:20:51 +0100

    Add bootstrap vue.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
 Vue.use(VueRouter)
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,13 @@
     <div id="app">
         <h1>Hello App!</h1>
         <p>
-            <!-- use router-link component for navigation. -->
-            <!-- specify the link by passing the `to` prop. -->
-            <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
-            <router-link to="/foo">Go to Foo</router-link>
-            <router-link to="/bar">Go to Bar</router-link>
-            <router-link to="/user/1">Go user 1</router-link>
-            <router-link to="/user/2">Go user 2</router-link>
+            <b-button to="/foo">Go to Foo</b-button>
+            <b-button to="/bar">Go to Bar</b-button>
+            <b-button to="/user/1">Go user 1</b-button>
+            <b-button to="/user/2">Go user 2</b-button>
         </p>
-        <!-- route outlet -->
-        <!-- component matched by the route will render here -->
         <router-view></router-view>
     </div>
 @endsection
+
+
